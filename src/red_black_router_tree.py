e22576d861b8d1a2ca8cc57fedcfd7df9de010a1
Enzo Lacerda <lacerdaenzo71@gmail.com>
2026-05-28 22:06:10 -0300

    feat: add left and right rotations for RBT

--- src/red_black_router_tree.py
+++ src/red_black_router_tree.py
@@ -52,3 +52,75 @@ class RedBlackRouterTree:
         # Executa o fixup se houver colisão de nós vermelhos
         if new_node.parent.color == RED:
             self._fix_insert(new_node)
+
+    def _fix_insert(self, k):
+        while k.parent.color == RED:
+            if k.parent == k.parent.parent.left:
+                uncle = k.parent.parent.right
+
+                if uncle.color == RED:
+                    k.parent.color = BLACK
+                    uncle.color = BLACK
+                    k.parent.parent.color = RED
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.right:
+                        k = k.parent
+                        self.left_rotate(k)
+                    k.parent.color = BLACK
+                    k.parent.parent.color = RED
+                    self.right_rotate(k.parent.parent)
+            else:
+                uncle = k.parent.parent.left
+
+                if uncle.color == RED:
+                    k.parent.color = BLACK
+                    uncle.color = BLACK
+                    k.parent.parent.color = RED
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.left:
+                        k = k.parent
+                        self.right_rotate(k)
+                    k.parent.color = BLACK
+                    k.parent.parent.color = RED
+                    self.left_rotate(k.parent.parent)
+            if k == self.root:
+                break
+        self.root.color = BLACK
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+
+        if y.left != self.Tnil:
+            y.left.parent = x
+        y.parent = x.parent
+
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+
+        y.left = x
+        x.parent = y
+
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+
+        if y.right != self.Tnil:
+            y.right.parent = x
+        y.parent = x.parent
+
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+
+        y.right = x
+        x.parent = y
